5b8eb853f8202f806f0bcc7fa404b0680c199570
Mikhail Lashukov <lashukovms@gmail.com>
2024-08-26 19:16:24 +0300

    fix widget file with PEP8

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,22 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(account_card: str) -> str:
     """
     Принимает один аргумент — строку, содержащую тип и номер карты или счета.
     Возвращает строку с замаскированным номером.
     """
 
-    index_space = account_card.rfind(' ')
+    index_space = account_card.rfind(" ")
 
     card_name = account_card[:index_space]
-    card_number = account_card[index_space + 1:]
+    card_number = account_card[index_space + 1 :]
 
-    if card_name == 'Счет':
-        return f'{card_name} {get_mask_account(card_number)}'
+    if card_name == "Счет":
+        return f"{card_name} {get_mask_account(card_number)}"
     else:
-        return f'{card_name} {get_mask_card_number(card_number)}'
+        return f"{card_name} {get_mask_card_number(card_number)}"
+
 
 def get_date(date_error: str) -> str:
     """
@@ -26,6 +28,4 @@ def get_date(date_error: str) -> str:
 
     date_fix = date_error[:10]
 
-    return '.'.join(date_fix.split("-")[::-1])
-
-print(get_date(input()))
+    return ".".join(date_fix.split("-")[::-1])
