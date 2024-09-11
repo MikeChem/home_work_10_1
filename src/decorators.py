17b2f2d856d8e3c8048b6c1ef4eb24cf2406bec4
Mikhail Lashukov <lashukovms@gmail.com>
2024-09-11 15:27:43 +0300

    new test and fix decorators

--- src/decorators.py
+++ src/decorators.py
@@ -10,30 +10,31 @@ def log(filename=None):
     def decorator(my_function):
         def wrapper(*args, **kwargs):
             log_str = ""
+            result = None
             try:
                 time_start = time()
-                my_function(*args, **kwargs)
+                result = my_function(*args, **kwargs)
                 time_end = time()
                 time_summ = time_end - time_start
                 log_str += f"my_function is ok\n"
 
             except Exception as e:
                 log_str += f"my_function error: {e} Inputs: ({args}, {kwargs}\n"
-
+                # print(filename)
             if filename:
                 with open(filename, "a") as f:
                     f.write(log_str)
             else:
                 print(log_str)
 
+            return result
         return wrapper
 
     return decorator
 
 
 @log()
-def my_function(x, y):
+def function(x, y):
     return x / y
 
-
-my_function(1, 0)
+print(function(1, 2))
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -21,3 +21,14 @@ def test_log_filename_success():
 
     with open("mylog.txt", "r") as f:
         assert f.readlines()[-1] == "my_function is ok\n"
+
+def test_log_error(capsys):
+    @log()
+    def div(a, b):
+        return a / b
+
+    div(2, 0)
+
+    captured = capsys.readouterr()
+    # print(captured.out)
+    assert captured.out == 'my_function error: division by zero Inputs: ((2, 0), {}\n\n'
